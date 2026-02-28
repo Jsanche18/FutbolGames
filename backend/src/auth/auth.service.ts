18e5884fccdf25c6c386a21eecf8a320a579d1e2
Jsanche18 <jsanchobelanche@gmail.com>
2026-02-28 11:13:29 +0100

    3.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../common/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 import { ConfigService } from '@nestjs/config';
 import { randomUUID } from 'crypto';
 
